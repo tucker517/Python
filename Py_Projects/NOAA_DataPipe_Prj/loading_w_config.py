46a88417c8ea2d995219169b72ab856846546b18
tucker517 <61562279+tucker517@users.noreply.github.com>
2020-10-19 13:59:05 -0600

    Update loading_w_config.py

--- Py_Projects/NOAA_DataPipe_Prj/loading_w_config.py
+++ Py_Projects/NOAA_DataPipe_Prj/loading_w_config.py
@@ -1,21 +1,22 @@
-from pyodbc import connect as con
+from pymongo import MongoClient
 import pandas as pd
 
 
-class MS_SQLConn():
+class MongoDB:
     # Initialize the common usable variables in below function:
-    def __init__(self, username, password, host, db_name, port):
+    def __init__(self, username, password, host, db_name, port='27017', authSource='admin'):
         self.username = username
         self.password = password
         self.host = host
         self.port = port
         self.db_name = db_name
-        self.uri = 'DRIVER={ODBC Driver 17 for SQL Server};'+';SERVER='+ self.host+';DATABASE='+self.db_name+';UID='+self.username+';PWD='+self.password
-        
+        self.authSource = authSource
+        self.uri = 'mongodb://' + self.user + ':' + self.password + '@'+ self.host + ':'+ self.port + '/' + self.db_name + '?authSource=' + self.authSource
+       
         try:
-            self.client = con(self.uri)
+            self.client = MongoClient(self.uri)
             self.db = self.client[self.db_name]
-            print ("MS SQL Server Connection Successful.")
+            print ("MongoDB Connection Successful!")
         except Exception as e:
             print("Connection unsuccessful... Please try again...")
             print(e)
@@ -24,18 +25,26 @@ class MS_SQLConn():
 def insert_into_db(self, data, collection):
     if isinstance(data, pd.DataFrame):
         try:
-            self.db[collection]._insert_many(data.to_dict('records'))
+            self.db[collection].insert_many(data.to_dict('results'))
             print('Data Inserted Successfully')
         
         except Exception as e:
             print('OOPS!!! An error occurred while inserting the data into the database...')
             print(e)
-            
+        
+        else:
+            try:
+                self.db[collection].insert_many(data)
+                print('Data Inserted Successfully!')
+            except Exception as e:
+                print("OOPS! An error occurred inserting the data...") 
+                print(e)
+                
 def read_from_db(self, collection):
     try:
         data = pd.DataFrame(list(self.db[collection].find()))  
         print('Data Fetched Successfully')
         return data
     except Exception as e:
-        print("OOPS!! An error occurred while retreiving the data from the api")
+        print("OOPS!! An error occurred while pulling the data from the api")
         print(e)     
