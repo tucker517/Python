9feecd2428b42a270ea650f34020395f9d6a1968
tucker517 <61562279+tucker517@users.noreply.github.com>
2020-11-02 11:54:47 -0700

    Update json_techniques2.py

--- Py_Projects/Learning_Modules/JSON/json_techniques2.py
+++ Py_Projects/Learning_Modules/JSON/json_techniques2.py
@@ -3,16 +3,13 @@
 import json
 
 
-# We first open the json file as variable f and set data = json.load(f)
-# We simply load the initial json file
+# How to load a json file
 with open('states.json') as f:
     data = json.load(f)
 
 for state in data['states']:
     del state['area_codes']
 
-# Just as before we need to open a new file called new_states.json
-# You must use the 'w' to write the file out and you have created a
-# new json file voila!
+# Create a new json file with the updated data use 'w' to specify write
 with open('new_states.json', 'w') as f:
     json.dump(data, f, indent=2)
