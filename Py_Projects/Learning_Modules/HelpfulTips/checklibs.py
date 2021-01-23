f433332b992446137cb61022eb8759f9f2f9b27d
tucker517 <61562279+tucker517@users.noreply.github.com>
2021-01-23 11:10:58 -0700

    Add files via upload

---
+++ Py_Projects/Learning_Modules/HelpfulTips/checklibs.py
@@ -0,0 +1,26 @@
+# # Check python libs and versions
+# # scipy
+# import scipy
+# print('scipy: %s' % scipy.__version__)
+# # numpy
+# import numpy
+# print('numpy: %s' % numpy.__version__)
+# # matplotlib
+# import matplotlib
+# print('matplotlib: %s' % matplotlib.__version__)
+# # pandas
+# import pandas
+# print('pandas: %s' % pandas.__version__)
+# # statsmodels
+# import statsmodels
+
+
+# For 32 bit it will return 32 and for 64 bit it will return 64
+# import struct
+# print(struct.calcsize("P") * 8)
+# print('statsmodels: %s' % statsmodels.__version__)
+# # scikit-learn
+# import sklearn
+# print('sklearn: %s' % sklearn.__version__)
+
+
